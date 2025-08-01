82d49ca8567fa1ff266d61acbb6ddf1891e2c3e3
evallugo <lugoe@rpi.edu>
2025-08-01 16:22:55 -0400

    begin

--- index.php
+++ index.php
@@ -4,5 +4,16 @@
   </head>
   <body>
    <h2 id="loginbg">Log In<h2>
+
+<form action="/dashboard.php">
+  <label for="uname">Username:</label><br>
+  <input type="text" id="uname"><br>
+  <label for="pass">Password:</label><br>
+  <input type="text" id="pass"><br>
+  <input type="li" value="Log In">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
   </body>
 </html>
