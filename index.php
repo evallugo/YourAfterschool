273aefbef6246cf18e1d4883d69c9e6f073655c5
evallugo <lugoe@rpi.edu>
2025-08-01 16:12:48 -0400

    login

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
     <link rel="stylesheet" href="home.css">
   </head>
   <body>
-    <div class="login-background">
-      <div class="outline">
-        <div class="logo"></div>
-        <div class="username-box"></div>
-        <div class="password-box"></div>
-        <div class="signin-box"></div>
-      </div>
-    </div>
+   <h2 id="loginbg">Log In<h2>
   </body>
 </html>
